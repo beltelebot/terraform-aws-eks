bea797ea7d058b2de519072756787194864bc9e0
Beltelebot <60689861+beltelebot@users.noreply.github.com>
2021-03-09 17:06:54 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,23 @@ resource "random_string" "eksname" {
   special          = false
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.control_plane.cluster_id
+}
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.control_plane.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
+  
+  
 module "control_plane" {
   source = "./modules/control_plane"
 
